b6938535d3154e1ebbb4545b7f722409bde573cc
faiyaz1505 <fasc786@gmail.com>
2023-10-14 23:08:25 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+	
+	private int aid;
+	private String aname;
+	private String tech;
+	
+	
+	public Alien() {
+		System.out.println("constructor");
+	}
+	@Autowired
+	private Laptop laptop;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	
+	
+	
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public void show() {
+		System.out.println("alien class method");
+		laptop.compile();
+	}
+
+}
